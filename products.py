a225526c2f22f05d6d6ddc4e1630479f52563906
Charlene Lo <lo861119@gmail.com>
2022-05-07 14:14:49 +0800

    write csv and txt

--- products.py
+++ products.py
@@ -1,4 +1,4 @@
-
+# 請使用者輸入產品價格資訊
 products = []
 while True:
 	name = input('請輸入商品名稱:')
@@ -11,5 +11,18 @@ while True:
 	products.append([name, price])
 print(products)
 
+
+# 看每一個價格
 for p in products:
 	print(p[0], '的價格是', p[1])
+
+
+# 寫入text
+with open('products.txt', 'w') as f:
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
+
+# 寫入csv
+with open('products.csv', 'w') as f:
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
